84e0c4d1f518e1b29a7798bea47542e8e7955233
HamzaFikri <mohammed.hamza.fikri@gmail.com>
2024-01-23 18:38:13 +0100

    Top students

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -2,6 +2,13 @@
 """ Module for using PyMongo """
 
 
+import pymongo
+
+
 def top_students(mongo_collection):
-    """ Returns all students sorted by average score"""
-    return list(mongo_collection.find())
+    """Function that returns all students sorted by average score"""
+
+    return mongo_collection.aggregate(
+            [{"$project": {
+                "name": "$name", "averageScore": {"$avg": "$topics.score"}}},
+                {"$sort": {"averageScore": -1}}])
