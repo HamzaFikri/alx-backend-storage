776af44db8dba4d294fbced72d9959f13495adb7
HamzaFikri <mohammed.hamza.fikri@gmail.com>
2024-01-25 01:21:57 +0100

    Advanced task

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,18 @@
 #!/usr/bin/env python3
-"""
-web cache and tracker
-"""
+"""module for the function get_page"""
+
 import requests
 import redis
-from functools import wraps
 
-store = redis.Redis()
-
-
-def count_url_access(method):
-    """ Decorator counting how many times
-    a URL is accessed """
-    @wraps(method)
-    def wrapper(url):
-        cached_key = "cached:" + url
-        cached_data = store.get(cached_key)
-        if cached_data:
-            return cached_data.decode("utf-8")
-
-        count_key = "count:" + url
-        html = method(url)
-
-        store.incr(count_key)
-        store.set(cached_key, html)
-        store.expire(cached_key, 10)
-        return html
-    return wrapper
+client = redis.Redis()
 
 
-@count_url_access
 def get_page(url: str) -> str:
-    """ Returns HTML content of a url """
-    res = requests.get(url)
-    return res.text
+    """obtain the HTML content of a particular URL and returns it."""
+    result = requests.get(url).text
+    if not client.get("count:{}".format(url)):
+        client.set("count:{}".format(url), 1)
+        client.setex("result:{}".format(url), 10, result)
+    else:
+        client.incr("count:{}".format(url), 1)
+    return result
